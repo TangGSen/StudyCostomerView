9c4d0a9d8b05934f89a703dbaa9c8d73c7dc28d1
Jasonsens17@gmail.com <Jasonsens17@gmail.com>
2016-07-07 12:18:02 +0800

    good

--- customerviewtest/src/main/java/sen/com/customerviewtest/test8/MutilCircleView.java
+++ customerviewtest/src/main/java/sen/com/customerviewtest/test8/MutilCircleView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -28,6 +29,8 @@ public class MutilCircleView extends View {
     private int ccX;
     private float ccY;
     private TextPaint mTextPaint;
+    private float textOffsetY;
+    private Paint mArcPaint;
 
     public MutilCircleView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -46,6 +49,10 @@ public class MutilCircleView extends View {
         mTextPaint.setColor(Color.WHITE);
         mTextPaint.setTextSize(26);
         mTextPaint.setTextAlign(Paint.Align.CENTER);
+        textOffsetY = (mTextPaint.descent() + mTextPaint.ascent()) / 2;
+
+        //弧形画笔
+        mArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
 
     }
 
@@ -90,18 +97,19 @@ public class MutilCircleView extends View {
         super.onDraw(canvas);
         canvas.drawColor(0xFFF29B76);
         canvas.drawCircle(ccX, ccY, largeCricleRadiu, mStorkePaint);
-        canvas.drawText("JseoniY",ccX ,ccY,mTextPaint);
+        canvas.drawText("JseoniY",ccX ,ccY-textOffsetY,mTextPaint);
 //把360 平均分成5分，每份72 度
-        drawLineCircle(canvas,-78,false, new String[]{"JseonttT"});
-        drawLineCircle(canvas,-6,false, new String[]{"Jseondd"});
-        drawLineCircle(canvas,66,false, new String[]{"JseonionTT"});
-        drawLineCircle(canvas,138,false, new String[]{"JseonYY"});
-        drawLineCircle(canvas,210,true, new String[]{"Jseon","senTang"});
+        drawLineCircle(canvas,-78,false, new String[]{"JseonttT"},true);
+        //在这个画弧形
+        drawLineCircle(canvas,-6,false, new String[]{"Jseondd"},false);
+        drawLineCircle(canvas,66,false, new String[]{"JseonionTT"},false);
+        drawLineCircle(canvas,138,false, new String[]{"JseonYY"},false);
+        drawLineCircle(canvas,210,true, new String[]{"Jseon","senTang"},false);
 
 
     }
 
-    private void drawLineCircle(Canvas canvas, int r, boolean isMore, String[] strings) {
+    private void drawLineCircle(Canvas canvas, int r, boolean isMore, String[] strings, boolean isDrawArc) {
         //锁定画布
         canvas.save();
         canvas.translate(ccX, ccY);
@@ -109,20 +117,65 @@ public class MutilCircleView extends View {
         canvas.drawLine(largeCricleRadiu, 0, largeCricleRadiu * 2, 0, mStorkePaint);
         canvas.drawCircle(largeCricleRadiu * 3, 0, largeCricleRadiu, mStorkePaint);
 
-        
-        canvas.drawText(strings[0],largeCricleRadiu * 3,0,mTextPaint);
+        canvas.save();
+        canvas.translate(largeCricleRadiu * 3,0);
+        canvas.rotate(-r);
+        canvas.drawText(strings[0],0,0-textOffsetY,mTextPaint);
+        canvas.restore();
         if (isMore){
             canvas.drawLine(largeCricleRadiu*4, 0, largeCricleRadiu * 5, 0, mStorkePaint);
             canvas.drawCircle(largeCricleRadiu * 6, 0, largeCricleRadiu, mStorkePaint);
-            canvas.drawText(strings[1],largeCricleRadiu * 6,0,mTextPaint);
+            //为了纠正文字水平
+            canvas.save();
+            canvas.translate(largeCricleRadiu * 6,0);
+            canvas.rotate(-r);
+            canvas.drawText(strings[1],0,0-textOffsetY,mTextPaint);
+            canvas.restore();
+        }
+        if (isDrawArc){
+            //弧形的圆心就是这个圆心
+            drawArc(canvas,largeCricleRadiu * 3,r);
         }
 
         // 释放画布
         canvas.restore();
     }
 
+    private void drawArc(Canvas canvas,  float arcX,float r) {
+        canvas.save();
+        //把圆心作为起点
+        canvas.translate(arcX,0);
+
+        canvas.rotate(-r);
+
+        float arcRudic = size *ARC_RADIU ;
+        //这个 弧形在这个矩形内
+        RectF oval = new RectF(-arcRudic ,-arcRudic ,arcRudic ,arcRudic) ;
+
+        mArcPaint.setStyle(Paint.Style.STROKE);
+        mArcPaint.setStrokeWidth(3.0f);
+        mArcPaint.setColor(Color.WHITE);
+        // 第四个参数是true的时候画扇形，是false的时候画弧线
+        canvas.drawArc(oval ,-30 ,-120 ,false ,mArcPaint);
+
+        mArcPaint.setStyle(Paint.Style.FILL);
+        mArcPaint.setColor(0x55EC6941);
+        // 第四个参数是true的时候画扇形，是false的时候画弧线
+        canvas.drawArc(oval ,-30 ,-120 ,true ,mArcPaint);
+
+        //再画弧形上的字体,等分五分，那么没30 话文字
+        canvas.save();
+        //把画布旋转到最左
+        canvas.rotate(120/2);
+
+        for (int i =0 ;i<5 ;i++){
+
+        }
 
 
+        canvas.restore();
+
+    }
 
 
 
